64fc05e43abb1f3467f67d05cb1f5f01af303db8
erikgalaz1126-dot <erikgalaz1126@gmail.com>
2025-11-16 15:33:15 -0600

    Refactor warshall function for clarity and correctness
    Refactor Warshall function to use 'graph' parameter and update reach matrix assignment.

--- Warshall.py
+++ Warshall.py
@@ -1,23 +1,10 @@
-def warshall(reach):
-    n = len(reach)
-    R = [fila[:] for fila in reach]
+def warshall(graph):
+    num_vertices = len(graph)
+    reach = [fila[:] for fila in graph]
 
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                R[i][j] = R[i][j] or (R[i][k] and R[k][j])
-
-    return R
-
-
-# EJEMPLO
-reach = [
-    [1, 1, 0],
-    [0, 1, 1],
-    [0, 0, 1]
-]
-
-res = warshall(reach)
-print("Warshall (alcanzabilidad):")
-for fila in res:
-    print(fila)
+    for k in range(num_vertices):
+        for i in range(num_vertices):
+            for j in range(num_vertices):
+                if reach[i][k] and reach[k][j]:
+                    reach[i][j] = 1
+    return reach
