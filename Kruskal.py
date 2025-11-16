834b2d6a9c1613308dbbdbf4764e674de9428ddd
erikgalaz1126-dot <erikgalaz1126@gmail.com>
2025-11-16 15:32:45 -0600

    Refactor Kruskal's algorithm implementation

--- Kruskal.py
+++ Kruskal.py
@@ -1,47 +1,28 @@
-class UnionFind:
-    def __init__(self, n):
-        self.padre = list(range(n))
-        self.rango = [0] * n
+def kruskal(graph):
+    edges = []
+    num_vertices = len(graph)
 
-    def find(self, x):
-        if self.padre[x] != x:
-            self.padre[x] = self.find(self.padre[x])
-        return self.padre[x]
+    for i in range(num_vertices):
+        for j in range(i + 1, num_vertices):
+            if graph[i][j] != 0:
+                edges.append((graph[i][j], i, j))
 
-    def union(self, a, b):
-        ra, rb = self.find(a), self.find(b)
-        if ra != rb:
-            if self.rango[ra] < self.rango[rb]:
-                self.padre[ra] = rb
-            elif self.rango[ra] > self.rango[rb]:
-                self.padre[rb] = ra
-            else:
-                self.padre[rb] = ra
-                self.rango[ra] += 1
-            return True
-        return False
+    edges.sort()
 
+    parent = list(range(num_vertices))
 
-def kruskal(n, aristas):
-    # aristas = (peso, u, v)
-    aristas.sort()
-    uf = UnionFind(n)
+    def find(x):
+        while parent[x] != x:
+            x = parent[x]
+        return x
+
+    def union(x, y):
+        parent[find(x)] = find(y)
+
     mst = []
-
-    for peso, u, v in aristas:
-        if uf.union(u, v):
-            mst.append((u, v, peso))
+    for w, u, v in edges:
+        if find(u) != find(v):
+            mst.append((u, v, w))
+            union(u, v)
 
     return mst
-
-
-# EJEMPLO
-aristas = [
-    (1, 0, 1),
-    (4, 0, 2),
-    (2, 1, 2),
-    (5, 1, 3),
-    (3, 2, 3)
-]
-
-print("Kruskal (MST):", kruskal(4, aristas))
