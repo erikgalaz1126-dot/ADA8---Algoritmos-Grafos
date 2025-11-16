ed078eddb70b601fe3a8cf57f95850017ddee604
erikgalaz1126-dot <erikgalaz1126@gmail.com>
2025-11-16 15:32:05 -0600

    Refactor Floyd-Warshall function and update example

--- Floyd–Warshall.py
+++ Floyd–Warshall.py
@@ -1,26 +1,14 @@
-def floyd_warshall(mat):
-    n = len(mat)
-    dist = [fila[:] for fila in mat]
+def floyd(graph):
+    num_vertices = len(graph)
+    dist = [fila[:] for fila in graph]
 
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
+    for k in range(num_vertices):
+        for i in range(num_vertices):
+            for j in range(num_vertices):
                 if dist[i][k] + dist[k][j] < dist[i][j]:
                     dist[i][j] = dist[i][k] + dist[k][j]
 
     return dist
 
-
-# EJEMPLO: INF = 9999
-INF = 9999
-mat = [
-    [0,   3, INF,  7],
-    [8,   0,   2, INF],
-    [5, INF,   0,   1],
-    [2, INF, INF,  0]
-]
-
-res = floyd_warshall(mat)
-print("Floyd-Warshall:")
-for fila in res:
     print(fila)
+
